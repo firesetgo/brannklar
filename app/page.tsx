4447920068e828d7068ae016084c846419c4cc42
Brannklar <kontakt@brannklar.no>
2026-04-04 01:49:56 +0200

    Ryddet og oppdatert forside

--- app/page.tsx
+++ app/page.tsx
@@ -11,7 +11,7 @@ export default function HomePage() {
             alt="Brannklar logo"
             width={300}
             height={300}
-            className="h-12 w-auto sm:h-14"
+            className="h-20 w-auto sm:h-14"
             priority
           />
           <span className="text-2xl font-bold text-red-600 sm:text-3xl">
